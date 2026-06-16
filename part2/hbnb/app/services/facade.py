e79d00833f2fc4557c21244ae832b5fe40f5dd85
mkryan00 <13073@holbertonstudents.com>
2026-06-16 22:20:40 +0000

    Added facade logic for amenity

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,5 @@
 from app.persistence.repository import InMemoryRepository
-
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -20,17 +20,17 @@ class HBnBFacade:
 
     # Amenity methods
     def create_amenity(self, amenity_data):
-        # Placeholder for logic to create an amenity
-        pass
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
     def get_amenity(self, amenity_id):
-        # Placeholder for logic to retrieve an amenity by ID
-        pass
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-        # Placeholder for logic to retrieve all amenities
-        pass
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        # Placeholder for logic to update an amenity
-        pass
+        amenity = self.amenity_repo.get(amenity_id)
+        if amenity:
+            amenity.update(amenity_data)
