27b07ccf0639eace5e4b905bd0ee1f2349129e7d
Sebastion Price <sebastionprice@gmail.com>
2026-06-21 19:37:00 +1000

    Add api_model to main api

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.api_models import api as models_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -16,6 +17,7 @@ def create_app() -> Flask:
         doc="/api/v1/",
     )
 
+    api.add_namespace(models_ns)
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -1,64 +1,12 @@
 """api/v1/places api endpoint."""
 
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource
 from app.services import facade
 from app.api.v1.api_models import (
-    amenity_model_place as amenity_model,
-    user_model_full as user_model,
-    review_model_place as review_model,
     place_model,
 )
 
 api = Namespace("places", description="Place operations")
-
-# Define models for related entities
-amenity_model = api.model(
-    "PlaceAmenity",
-    {
-        "id": fields.String(description="Amenity ID"),
-        "name": fields.String(description="Name of the amenity"),
-    },
-)
-
-user_model = api.model(
-    "PlaceUser",
-    {
-        "id": fields.String(description="User ID"),
-        "first_name": fields.String(description="First name of the owner"),
-        "last_name": fields.String(description="Last name of the owner"),
-        "email": fields.String(description="Email of the owner"),
-    },
-)
-
-# review model
-
-review_model = api.model(
-    "PlaceReview",
-    {
-        "id": fields.String(description="Review ID"),
-        "text": fields.String(description="Text of the review"),
-        "rating": fields.Integer(description="Rating of the place"),
-        "user_id": fields.String(description="User ID"),
-    },
-)
-
-# Define the place model for input validation and documentation
-place_model = api.model(
-    "Place",
-    {
-        "title": fields.String(required=True, description="Title of the place"),
-        "description": fields.String(description="Description of the place"),
-        "price": fields.Float(required=True, description="Price per night"),
-        "latitude": fields.Float(required=True, description="Latitude of the place"),
-        "longitude": fields.Float(required=True, description="Longitude of the place"),
-        "owner_id": fields.String(required=True, description="ID of the owner"),
-        "amenities": fields.List(
-            fields.String,
-            required=True,
-            description="List of amenities IDs",
-        ),
-    },
-)
 
 
 @api.route("/")
@@ -146,8 +94,10 @@ class PlaceResource(Resource):
             if place.owner
             else None,
             "amenities": [
-                {"id": facade.get_amenity(
-                    a).id, "name": facade.get_amenity(a).name}
+                {
+                    "id": facade.get_amenity(a).id,
+                    "name": facade.get_amenity(a).name,
+                }
                 for a in getattr(place, "amenities", [])
             ],
         }, 200
